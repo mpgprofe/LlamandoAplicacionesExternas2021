b20e400eb2fd2906c2e25fafab138eee5afa62fc
Manuel Profesor <mpgprofe@iesflorenciopintado.es>
2021-09-30 10:23:16 +0200

    diferencia de fechas

--- app/src/main/java/com/example/llamandoaplicacionesexternas2021/MainActivity.java
+++ app/src/main/java/com/example/llamandoaplicacionesexternas2021/MainActivity.java
@@ -14,10 +14,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonLanzar,buttonMapa, buttonWhatsApp;
-    EditText editTextWeb;
+    Button buttonLanzar, buttonMapa, buttonWhatsApp, buttonEnviarEmail;
+    EditText editTextWeb, editTextAsunto, editTextCuerpo, editTextEmail;
     String coordenadasPeñarroya = "38.2937926,-5.2583539";
 
     @Override
@@ -29,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
         editTextWeb = findViewById(R.id.editTextWeb);
         buttonMapa = findViewById(R.id.buttonMapa);
         buttonWhatsApp = findViewById(R.id.buttonWhatsApp);
+        buttonEnviarEmail = findViewById(R.id.buttonEnviarMail);
+        editTextAsunto = findViewById(R.id.editTextAsunto);
+        editTextCuerpo = findViewById(R.id.editTextCuerpo);
+        editTextEmail = findViewById(R.id.editTextTextEmailAddress);
 
         buttonLanzar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("geo:"+coordenadasPeñarroya));
+                intent.setData(Uri.parse("geo:" + coordenadasPeñarroya));
                 startActivity(intent);
             }
         });
@@ -61,10 +67,11 @@ public class MainActivity extends AppCompatActivity {
                 intent.setType("text/plain");
                 intent.putExtra(Intent.EXTRA_TEXT, "Hola qué tal?");
                 intent.setPackage("com.whatsapp");
+                startActivity(intent);
 
-                if (getPackageManager().getLaunchIntentForPackage("com.whatsapp")!=null) {
+                if (getPackageManager().getLaunchIntentForPackage("com.whatsapp") != null) {
                     startActivity(intent);
-                }else{
+                } else {
                     Toast.makeText(MainActivity.this, "Instala whastsapp para que rule", Toast.LENGTH_LONG).show();
                 }
 
@@ -72,6 +79,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        buttonEnviarEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentoEnviarEmail = new Intent(Intent.ACTION_SEND);
+                intentoEnviarEmail.putExtra(Intent.EXTRA_EMAIL, new String[]{editTextEmail.getText().toString()});
+                intentoEnviarEmail.putExtra(Intent.EXTRA_SUBJECT, editTextAsunto.getText().toString());
+                intentoEnviarEmail.putExtra(Intent.EXTRA_TEXT, editTextCuerpo.getText().toString());
+                intentoEnviarEmail.setType("message/rfc822");
+                try {
+                    startActivity(Intent.createChooser(intentoEnviarEmail, "Escoge tu programa favorito de envío de emails"));
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(MainActivity.this, "Intala una app de envío de email", Toast.LENGTH_LONG).show();
+                }
 
+            }
+        });
+
+
+    }
+
+    private long diasParaMiCumple(){
+        Calendar hoy = Calendar.getInstance();
+        int año = 2022;
+        int mes = 6; //0 enero -- 11 diciembre
+        int dia = 28;
+        Calendar miCumple = Calendar.getInstance();
+        miCumple.set(año, mes, dia);
+
+        long diferenciaEnMilisegundos = miCumple.getTimeInMillis() - hoy.getTimeInMillis();
+
+        long diferenciaEnDías = diferenciaEnMilisegundos/1000/60/60/24;
+
+        return diferenciaEnDías;
+
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Toast.makeText(MainActivity.this, "Días para mi cumple: "+diasParaMiCumple(), Toast.LENGTH_SHORT).show();
     }
 }
